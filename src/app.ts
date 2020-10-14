412db90f24e7e984b589a216ecc5da3a0b190472
Simon <simonaverhoeven@gmail.com>
2020-10-15 00:04:07 +0800

    find avaiable slots PoC

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,14 @@
 import Axios from 'axios';
-import JSSoup from 'jssoup';
+import url from 'url';
+import moment from 'moment';
 
-const calendarName = 'swpsystemc';
+const JSSoup = require('jssoup').default;
 
 interface ICalendar {
   cal: string;
-  calendarIni: string;
-  calendarService: string;
+  ini: string;
+  service: string;
+  jumpDate?: string;
 }
 
 interface ICalendars {
@@ -14,48 +16,101 @@ interface ICalendars {
   swpsystemc: ICalendar;
 }
 
+interface ISlot {
+  startTime: string;
+  bookingLink: string;
+}
+
 const calendars = {
   swpsystemb: {
     cal: '21617379-33ba-41d3-b341-50b7fac01693',
-    calendarIni: '1596626159288',
-    calendarService: 'jsid21231',
+    ini: '1596626159288',
+    service: 'jsid21231',
   },
   swpsystemc: {
     cal: '693eae8c-0144-407d-92f2-725867e5be04',
-    calendarIni: '1596625182764',
-    calendarService: 'jsid8744009',
+    ini: '1602684055670',
+    service: 'jsid8744009',
   },
 };
 
-const axios = Axios.create({
-  baseURL: `https://${calendarName}.youcanbook.me`,
-});
+const fetch = async (calendarName: keyof ICalendars, dayInc: number | null = null) => {
+  const calendar = calendars[calendarName];
+  let axios = Axios.create({
+    baseURL: getBaseUrl(calendarName),
+  });
+  // const jumpDate = '2020-10-21';
+  const params: ICalendar = calendar;
 
-const fetch = async (calendar: ICalendar) => {
-  const res = await axios.get('/service/jsps/cal.jsp', { params: calendar });
+  if (dayInc) {
+    const jumpDate = moment().add(dayInc, 'days').format('YYYY-MM-DD');
+
+    console.log('jumpDate');
+    console.log(jumpDate);
+    params.jumpDate = jumpDate;
+  }
+
+  const res = await axios.get('/service/jsps/cal.jsp', { params });
+
+  console.log(res.request.res.responseUrl);
 
   return res.data;
 };
 
-const main = async (calendarName: keyof ICalendars) => {
-  const calendar = calendars[calendarName];
-  const res = await fetch(calendar);
-
-  var soup = new JSSoup(res);
-
-  const body = soup.find('body');
-
-  const dayElems = body.findAll('div', 'gridDay');
-
-  console.log('No of days', dayElems.length);
-
-  // console.log(dayElems);
-
-  return res;
+const getBaseUrl = (calendarName: string) => {
+  return `https://${calendarName}.youcanbook.me`;
 };
 
-main('swpsystemc').catch((e) => console.error(e));
+const mapToAvailableSlots = (response: any) => {
+  var soup = new JSSoup(response);
+  const body = soup.find('body');
+  const dayElems = body.findAll('div', 'gridDay') as any[];
+};
 
-// axios.get
+const main = async (calendarName: keyof ICalendars) => {
+  const availableSlots = [] as ISlot[];
 
-console.log('Hello World');
+  let dayElements = [] as any[];
+
+  const responses = await Promise.all([fetch(calendarName), fetch(calendarName, 7)]);
+
+  responses.forEach((res) => {
+    var soup = new JSSoup(res);
+    const body = soup.find('body');
+    const dayElems = body.findAll('div', 'gridDay') as any[];
+    dayElements = dayElements.concat(dayElems);
+  });
+
+  console.log('No of days found', dayElements.length);
+
+  dayElements.forEach((day, i) => {
+    // const freeSlots = day.findAll('div', 'gridFree') as any[];
+    const freeSlots = day.findAll('div', { class: ['gridFree', 'gridSlot'] }) as any[];
+
+    freeSlots.forEach((freeSlot) => {
+      const linkElem = freeSlot.find('a');
+
+      // ToDo find proper way to this
+      const bookingLink = getBaseUrl(calendarName) + linkElem.attrs.href.replace(/&amp;/g, '&');
+      const bookingUrl = url.parse(bookingLink, true);
+
+      const bookingSlot = bookingUrl?.query?.slot;
+
+      const startTime = moment.unix(+bookingSlot / 1000).toString();
+      availableSlots.push({ startTime, bookingLink });
+      //
+    });
+
+    // console.log(i, freeSlots.length);
+  });
+
+  return availableSlots;
+};
+
+main('swpsystemc')
+  .then((availableSlots: ISlot[]) => {
+    console.log(availableSlots);
+  })
+  .catch((e) => console.error(e));
+
+console.log('Hello World 2');
